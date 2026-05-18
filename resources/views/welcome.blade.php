653a22c0f66e6acd1af58eeaf196a1b3168a3aff
Adams899591 <usmanadams551@gmail.com>
2026-05-17 19:35:10 -0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    @vite(["resources/css/app.css","resources/js/app.js"])
+    {{-- @vite("resources/css/app.js") --}}
+</head>
+<body>
+    
+    <h1>This is the welcome page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Corporis ea
+         sed, animi rem laudantium assumenda explicabo id suscipit nostrum adipisci, even
+        iet excepturi fugit magni. Doloremque eligendi illo accusamus id ducimus.</p>
+     {{-- Set-ExecutionPolicy -Scope Process -ExecutionPolicy Bypass --}}
+     <iframe src="" frameborder="0"></iframe>
+ 
+</body>
+</html>
